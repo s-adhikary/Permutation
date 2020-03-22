91eb8d59135f55488a04b9fb08214565a319c2ec
Supriyo Adhikary <61983291+crs1903@users.noreply.github.com>
2020-03-22 18:15:02 +0530

    Create permutation_with_repetition.c

---
+++ permutation_with_repetition.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+void print(void);
+void permute(int*,int,int);
+int main()
+{
+	int *arr,n;
+	printf("Enter the number of elements in the set:\n");
+	scanf("%d",&n);
+	arr=(int*)malloc(n*sizeof(int));
+	printf("Enter the Elements: [numeric]\n");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",arr+i);
+		
+	}
+	
+	permute(arr,0,n-1);
+}
+void permute(int* arr,int i,int t)
+{	
+	if(i==t)
+	{
+		for(int k=0;k<=t;k++)
+			printf("%d  ",arr[k]);
+		printf("\n");
+	}
+	int check=0;	
+	for(int k=i;k<=t;k++)
+	{
+		for(int m=i;m<k;m++)
+			if(arr[k]==arr[m])
+				check=1;
+		if(check==1)
+		{	
+			check=0;
+			continue;
+		}
+		int c=arr[i];
+		arr[i]=arr[k];
+		arr[k]=c;
+
+		permute(arr,i+1,t);
+
+		c=arr[i];
+		arr[i]=arr[k];
+		arr[k]=c;		
+	}
+}
